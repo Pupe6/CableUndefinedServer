2d7f6de8b806ac889bcfc7a85f41246b14504b3b
ProGamer2711 <alexandurshestakov@gmail.com>
2023-10-16 13:53:24 +0300

    switched to ts fully

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
-import { createServer } from "http";
+import dotenv from "dotenv";
+import cors from "cors";
 import express from "express";
+import { createServer } from "http";
 import { Server } from "socket.io";
-import { randomUUID } from "crypto";
+import mongoose from "mongoose";
+import path from "path";
+import fs from "fs";
+
+dotenv.config();
 
 const app = express();
 const server = createServer(app);
@@ -11,63 +17,84 @@ const io = new Server(server, {
 	},
 });
 
-app.get("/", (_, res) => {
-	res.json({ message: "Hello, World!" });
+const routesPath = path.join(__dirname, "routes");
+
+app.use(express.json());
+
+// Set CORS With Whitelist Array
+let whitelistedDomains = process.env.WHITELISTED_DOMAINS.split(", ");
+let allowedOrigins =
+	process.env.NODE_ENV === "production"
+		? whitelistedDomains
+		: // todo change port
+		  ["http://localhost:5500", "http://127.0.0.1:5500"];
+
+app.use(
+	cors({
+		origin: allowedOrigins,
+		optionsSuccessStatus: 200,
+	})
+);
+
+app.use((req, res, next) => {
+	const origin = req.headers.origin;
+
+	// todo might need to comment this out
+	if (allowedOrigins.includes(origin))
+		res.setHeader("Access-Control-Allow-Origin", "*");
+
+	// todo maybe change to *
+	res.setHeader(
+		"Access-Control-Allow-Methods",
+		"GET, POST, PUT, DELETE, OPTIONS"
+	);
+	res.setHeader(
+		"Access-Control-Allow-Headers",
+		"Content-Type, X-Token, Origin"
+	);
+
+	next();
 });
 
+// Register Routes
+try {
+	fs.readdirSync(routesPath).forEach(file => {
+		const route = require(path.join(routesPath, file));
+		app.use(route.path, route.router);
+	});
+
+	app.all("*", (_, res) =>
+		res.status(404).json({
+			message: "Not found",
+		})
+	);
+} catch (err) {
+	console.log(err);
+	app.all("*", (_, res) =>
+		res.status(500).json({
+			message: "Internal server error",
+		})
+	);
+}
+
 io.on("connection", socket => {
-	console.log(`Client connected with id: ${socket.id}`);
+	console.log("User connected");
 
-	// socket.on("message", ({ message, time }) => {
-	// 	console.log(`[${time}] Received message => ${message}`);
-
-	// 	// broadcast message to all connected clients
-	// 	socket.broadcast.emit("message", { message, time });
-	// });
-
-	// detect user joining a room
-	socket.on("join-room", roomId => {
-		socket.join(roomId);
-		console.log(`User joined room: ${roomId}`);
-
-		socket.on("message", ({ message, time }) => {
-			console.log(`[${time}] Received message => ${message}`);
-
-			// broadcast message to all connected clients
-			socket.broadcast.to(roomId).emit("message", { message, time });
-		});
-
-		socket.broadcast.to(roomId).emit("message", {
-			message: `User joined the room with id: ${roomId}`,
-			time: new Date().toLocaleTimeString(),
-		});
+	socket.on("disconnect", () => {
+		console.log("User disconnected");
 	});
 
-	socket.on("create-room", () => {
-		const roomId = randomUUID();
-
-		socket.join(roomId);
-		console.log(`User created room: ${roomId}`);
-
-		socket.on("message", ({ message, time }) => {
-			console.log(`[${time}] Received message => ${message}`);
-
-			// broadcast message to all connected clients
-			socket.broadcast.to(roomId).emit("message", { message, time });
-		});
-
-		socket.emit("message", {
-			message: `Room created with id: ${roomId}`,
-			time: new Date().toLocaleTimeString(),
-		});
-	});
-
-	socket.on("leave-room", roomId => {
-		socket.leave(roomId);
-		console.log(`User left room: ${roomId}`);
+	socket.on("error", err => {
+		console.log(err);
 	});
 });
 
 const port = process.env.PORT || 8393;
 
-server.listen(port, () => console.log(`Server started on port ${port}`));
+// Connect To MongoDB
+mongoose.connect(process.env.DB_URI).then(() => {
+	console.log("Connected to MongoDB");
+
+	// Start The Server
+	server.listen(port, () => console.log(`Server started on port ${port}`));
+});
